d6b8b6da35053b1ed878bbd9cae13e48842a10bf
Hassan Sarfraz <Hassan Sarfraz@DESKTOP-N4M2O6J>
2016-12-02 08:36:02 +0500

    commit2

--- del_serviceprovider.php
+++ del_serviceprovider.php
@@ -2,13 +2,15 @@
 
 include './connect.php';
 $spid=$_REQUEST['spid'];
+
+$pid = $_REQUEST['pid'];
 $query="delete from serviceprovider where spid=$spid";
 if (mysqli_query($link, $query)){
-    header("location:view_serviceprovider.php");
+    header("locaation:view_serviceprovider.php");
+    header(location:home.php);
 }
 else
 {
-    echo "Error : " . mysqli_error($link);
 }
 
 ?>
